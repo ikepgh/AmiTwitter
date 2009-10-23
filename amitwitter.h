a08fd162bfb3f4ce2c1e175e4a614573bbd6f1ff
ikepgh <ikepgh@7bbc0fec-c8ee-4298-833a-08a224068b25>
2009-10-23 17:21:28 +0000

    minor changes to reflect Open Src Project

--- amitwitter.h
+++ amitwitter.h
@@ -46,12 +46,15 @@
 "<HR>" \
 "<CENTER>The latest source is always available at:</CENTER><BR>" \
 "<CENTER>https://sourceforge.net/projects/amitwitter/</CENTER><BR>" \
+"<p>" \
+"<CENTER>-- Thanks for your interest, The AmiTwitter Open Source Team</CENTER><BR>" \
 "<HR>" \
 "<CENTER>This program uses:</CENTER><BR>" \
 "<CENTER>MUI © Stefen Stuntz</CENTER><BR>" \
 "<CENTER>URLtext © Alfonso Ranieri</CENTER><BR>" \
 "<CENTER>HTMLtext © Dirk Holtwick</CENTER><BR>" \
 "<CENTER>OpenURL, BetterString  © Open Source Team</CENTER><BR>" \
+"<CENTER>libiconv.ixlibrary © GNU, port by Bruno Haible</CENTER><BR>" \
 "<HR>" \
 "<CENTER><IMG SRC = PROGDIR:data/program_images/curl.gif>&nbsp;</CENTER><BR>" \
 "<BR>" \
@@ -67,31 +70,31 @@
 "<FONT COLOR=#000000><CENTER><B>FAQs </B></CENTER><BR>" \
 "<HR>" \
 "<CENTER>What are the requirements for AmiTwitter?</CENTER><BR>" \
-"<CENTER>Answer: MUI, HTMLtext, URLtext, BetterString.  OpenURL is optional. <BR>" \
+"<CENTER>Answer: MUI, HTMLtext, URLtext, BetterString.  'libiconv.ixlibrary' is required to be in your 'Libs' directory.  OpenURL is optional. </CENTER><BR>" \
 "<HR>"\
 "<CENTER>AmiTwitter does not download or send Tweets?</CENTER><BR>" \
 "<CENTER>Answer:  Are you connected to the internet, have you entered your Twitter username/password in the 'Tools' ->'Settings' window and saved them?</CENTER><BR>" \
 "<HR>" \
 "<CENTER>Direct Messages don't seem to work?</CENTER><BR>" \
-"<CENTER>Answer: Ensure you enter the recipient's 'Screen Name' (i.e. username) correctly, it does not currently accept user id numbers.<BR>" \
+"<CENTER>Answer: Ensure you enter the recipient's 'Screen Name' (i.e. username) correctly, it does not currently accept user id numbers.</CENTER><BR>" \
 "<HR>" \
-"<CENTER>I like AmiTwitter, how do I donate?<BR>" \
-"<CENTER>Answer: Follow the 'Click to Donate!' link to the PayPal website that opens in your favorite web browser<BR>" \
+"<CENTER>I like AmiTwitter, how do I donate?</CENTER><BR>" \
+"<CENTER>Answer: Follow the 'Click to Donate!' link to the PayPal website that opens in your favorite web browser</CENTER><BR>" \
 "<HR>" \
 "<CENTER>Nothing happens when I click on the links at the very bottom of the main program window?</CENTER><BR>" \
-"<CENTER>Answer: Ensure OpenURL is properly installed/configured.<BR>" \
+"<CENTER>Answer: Ensure OpenURL is properly installed/configured.</CENTER><BR>" \
 "<HR>" \
 "<CENTER>Why is the file size so large?</CENTER><BR>" \
-"<CENTER>Answer: Because AmiTwitter relies on numerous libs to work: cURL, SSL, xml2, glib, etc....<BR>" \
+"<CENTER>Answer: Because AmiTwitter relies on numerous libs to work: cURL, SSL, xml2, glib, etc....</CENTER><BR>" \
 "<HR>" \
 "<CENTER>Why did you write AmiTwitter?</CENTER><BR>" \
-"<CENTER>Answer: To prove it could be done on AmigaOS 3.x!<BR>" \
+"<CENTER>Answer: To prove it could be done on AmigaOS 3.x!</CENTER><BR>" \
 "<HR>" \
 "<CENTER>What is AmiTwitter based on?</CENTER><BR>" \
-"<CENTER>Answer: xTwitter by Tsukasa Hamano <BR>" \
+"<CENTER>Answer: xTwitter by Tsukasa Hamano </CENTER><BR>" \
 "<HR>" \
 "<CENTER>Links in the main Tweets window don't work?</CENTER><BR>" \
-"<CENTER>Answer: This is not a webbrowser...Also...it is a limitation of the .mcc's currently available for 3.x and useable with gcc (i.e. HTMLtext)<BR>" \
+"<CENTER>Answer: This is not a webbrowser...Also...it is a limitation of the .mcc's currently available for 3.x and useable with gcc (i.e. HTMLtext)</CENTER><BR>" \
 "<HR>" \
 "<CENTER>What are the  future plans for AmiTwitter?</CENTER><BR>" \
 "<CENTER>Answer: First, learn more of the Twitter API, but it depends on the feedback I recieve.  Also, ports to other systems (i.e MorphOS and OS 4.x) and an active source code base with some developers joining the project at SourceForge would be nice...<BR>" \
